1dc0ad0ef650993e760056fbba1fd44760febbbb
kunalevicshukla <kunal.shukla2002@gmail.com>
2024-11-04 22:26:32 +0530

    Update 4nov24.cpp

--- 4nov24.cpp
+++ 4nov24.cpp
@@ -19,3 +19,41 @@ public:
         return comp;
     }
 };
+
+/*
+
+class Solution {
+public:
+    string compressedString(string word) {
+        int count=1;
+        char prev=word[0];
+        string ans="";
+        for(int i=1;i<word.size();i++)
+        {
+          if(word[i]==prev)
+          {
+            count++;
+          }
+          else
+          {
+            ans+=to_string(count)+prev;
+            prev=word[i];
+            count=1;
+          }
+          if(count==9)
+          {
+            ans+=to_string(count)+prev;
+            count=1;
+            if(i+1<word.size())
+            {
+              prev=word[i+1];
+              i++;
+            }
+            
+          }
+        }
+        ans+=to_string(count)+prev;
+        return ans;
+    }
+};
+*/
